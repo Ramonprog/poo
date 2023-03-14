aec28e07add4a02eaee07e3df5077c0cf739d98b
ramonprog <programador.ramon@gmail.com>
2023-03-14 17:55:49 -0300

    abistracao

---
+++ src/ex1.ts
@@ -0,0 +1,13 @@
+class Carro {
+  cor = 'preto'
+  marca = 'Volkwagen'
+  modelo = 'fusca'
+  ano = 1970
+  potencia = 90
+}
+
+// instanciando classe para criar um novo obj
+
+const carro = new Carro()
+carro.cor = 'cinza'
+console.log(carro)
